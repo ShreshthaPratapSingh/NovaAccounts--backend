af19887e10eceba3583143bd5995be6260ca22d3
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-12-04 23:03:29 +0530

    Updated urls.py

--- NovaAccounts/NovaAccounts/urls.py
+++ NovaAccounts/NovaAccounts/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/', csrf_exempt(TokenObtainPairView.as_view()), name='token_obtain_pair'),
+    path('api/token/refresh/', csrf_exempt(TokenRefreshView.as_view()), name='token_refresh'),
 
     path('api/authentication/', include('authentication.urls')),
     path('api/transactions/', include("transactions.urls")),
