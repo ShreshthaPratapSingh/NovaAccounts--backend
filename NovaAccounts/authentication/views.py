3cb0e881ecc8bba4a5cc8aea922eb8ef8e8e90fa
ShreshthaPratapSingh <shreshthapratap0@gmail.com>
2025-12-05 00:48:14 +0530

    authentication update

--- NovaAccounts/authentication/views.py
+++ NovaAccounts/authentication/views.py
@@ -5,9 +5,32 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, UserSerializer
 
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        # Use existing serializer to create user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        # Generate fresh JWT tokens
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+
+        return Response({
+            "user": UserSerializer(user).data,
+            "refresh": str(refresh),
+            "access": str(access),
+        })
 
 class ProfileView(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticated]
